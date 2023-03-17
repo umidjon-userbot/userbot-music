2bb9ef42a9e50114ed0a4812a8a4732977e47dde
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2023-03-17 20:43:09 +0500

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,8 +1,18 @@
-# import logging
 from pyrogram import Client, idle
 
-app = Client("tgvc")
-# logging.basicConfig(level=logging.INFO)
+api_id = 1234567
+api_hash = "0123456789abcdef0123456789abcdef"
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc.player",
+        "ping",
+        "sysinfo"
+    ]
+)
+
+app = Client("tgvc", api_id, api_hash, plugins=plugins)
 app.start()
 print('>>> USERBOT STARTED')
 idle()
